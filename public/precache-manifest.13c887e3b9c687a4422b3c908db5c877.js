561eec307c3c12eea4925a2b3d368ee913c483cc
Himanshu Bhargava <himanshu.bhargava@cognizant.com>
2019-10-03 11:19:54 +0500

    Added react build

---
+++ public/precache-manifest.13c887e3b9c687a4422b3c908db5c877.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94e0d058f1efea0169fad6b33b074584",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c641adddc0e3f6b0fe8",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "032d9274dc04a8ef3719",
+    "url": "/static/css/main.31ef8399.chunk.css"
+  },
+  {
+    "revision": "1c641adddc0e3f6b0fe8",
+    "url": "/static/js/2.8e14d437.chunk.js"
+  },
+  {
+    "revision": "032d9274dc04a8ef3719",
+    "url": "/static/js/main.6879a07e.chunk.js"
+  },
+  {
+    "revision": "4745cd0c55cca41d5796",
+    "url": "/static/js/runtime~main.ef75cd9e.js"
+  },
+  {
+    "revision": "0e29c7461126c332633ba7764c7d2673",
+    "url": "/static/media/Bell.0e29c746.png"
+  },
+  {
+    "revision": "953c8396bf2e1fe477fc3c0d6cb90f1c",
+    "url": "/static/media/Mail.953c8396.png"
+  },
+  {
+    "revision": "db35f50bbc7d0818bcfd6e1e3816e0aa",
+    "url": "/static/media/Message.db35f50b.png"
+  },
+  {
+    "revision": "3f5764a2acf947147857e626c1bfae86",
+    "url": "/static/media/ShoppingCart.3f5764a2.png"
+  },
+  {
+    "revision": "82743ba4f5f8c2008d115e3c0ffd3e38",
+    "url": "/static/media/Whatsapp.82743ba4.png"
+  },
+  {
+    "revision": "c42c0c1e3559f89941712aaf41fbc722",
+    "url": "/static/media/course-image-1.c42c0c1e.jpg"
+  },
+  {
+    "revision": "f1cdf7dddfd83441c6e2add40dbd704c",
+    "url": "/static/media/course-image-2.f1cdf7dd.jpg"
+  }
+]);
